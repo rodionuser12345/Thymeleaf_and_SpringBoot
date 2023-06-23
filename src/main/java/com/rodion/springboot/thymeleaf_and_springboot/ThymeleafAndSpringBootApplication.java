ac6209eda6892d3241704a208647eaef93ec0276
rodionuser12345 <busurmanov.rodion@gmail.com>
2023-06-23 10:06:11 +0300

    Init

---
+++ src/main/java/com/rodion/springboot/thymeleaf_and_springboot/ThymeleafAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rodion.springboot.thymeleaf_and_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafAndSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafAndSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodion/springboot/thymeleaf_and_springboot/ThymeleafAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodion.springboot.thymeleaf_and_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafAndSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
